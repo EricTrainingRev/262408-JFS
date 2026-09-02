b844d6d46218c54a502b06019753d633c56a652b
EricTrainingRev <eric.suminski@revature.com>
2026-09-02 11:59:50 -0400

    adding some notes to the class example

--- Week-2/made-by-intellij/src/main/java/com/revature/classes/ClassBasics.java
+++ Week-2/made-by-intellij/src/main/java/com/revature/classes/ClassBasics.java
@@ -11,6 +11,7 @@ public class ClassBasics {
      */
     // this field is in the "instance" scope: it belongs to objects of the class
     int identifier;
+    // this field is in the "static" scope: it belongs to the class
     static int count = 0;
 
     public ClassBasics(){
@@ -37,22 +38,36 @@ public class ClassBasics {
         System.out.println("myObject identifier = " + myObject.identifier);
         System.out.println("anotherObject identifier = " + anotherObject.identifier);
 
+        // because we override the toString method we get a more human-friendly output of the object data
         System.out.println(myObject);
         System.out.println(anotherObject);
 
+        // note how any type of object can be passsed to the equals method: this means we must check the type of data first
         String stringObject = "This is a string";
         Integer intergerObject = Integer.valueOf(100);
         intergerObject.equals(null);
 
     }
 
+    /*
+        When creating your own classes you will have to determine what makes objects "equal" to each other. With modern
+        editors it is simple enough to compare all the fields of the objects
+     */
     @Override
     public boolean equals(Object o) {
+        // first check the object types are the same
         if (o == null || getClass() != o.getClass()) return false;
+        // if the same cast the raw Object as your Class type
         ClassBasics that = (ClassBasics) o;
+        // now you can check the fields match
         return this.identifier == that.identifier;
     }
 
+    /*
+        hash based collections use the hashCode method to store objects in a way that is quick to access and store. The
+        rule of thumb you should follow is that any objects that are equal (as determined by the equals method) should
+        return the same hashCode.
+     */
     @Override
     public int hashCode() {
         return Objects.hashCode(identifier);
