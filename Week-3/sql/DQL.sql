d9f10964532c28cc08419ed811f3f1058e4bb164
EricTrainingRev <eric.suminski@revature.com>
2026-09-09 12:54:43 -0400

    adding more DQL notes

--- Week-3/sql/DQL.sql
+++ Week-3/sql/DQL.sql
@@ -28,3 +28,87 @@ from Employee e
 join Customer c on e.EmployeeId = c.SupportRepId 
 group by employee
 order by employee asc;
+-- sum is also a good aggregate function to keep in mind for QC
+
+-- scalar functions work on individual data points. Here each record will have its FirstName
+-- column in all lowercase, the LastName column in all uppercase
+select lower(FirstName), upper(LastName) from Employee;
+
+-- here we calculate how much each employee has assisted on in sales
+-- note how we filter the results to return only those who made more than 750 in total sales
+select
+	e.FirstName || ' ' || e.LastName as employee,
+	sum(i.Total) as sales
+from Employee e
+join Customer c on e.EmployeeId = c.SupportRepId 
+join Invoice i on c.CustomerId = i.CustomerId 
+group by employee
+having sales >= 750 -- note we use "having" instead of "where" to filter aggregated results
+order by employee asc;
+
+-- here we can see we can join on many tables together to get access to the data
+-- and the organization of the data we need
+SELECT 
+	t.Name as track,
+	a.Title as album,
+	ar.Name as artist,
+	g.Name as genre
+from Track t 
+join Album a on t.AlbumId  = a.AlbumId
+join Artist ar on a.ArtistId = ar.ArtistId 
+join Genre g on t.GenreId = g.GenreId 
+order by ar.name asc;
+
+-- we can get all the data from the "left" table whether or not there is a match
+select 
+	e.FirstName || ' ' || e.LastName as reportee, 
+	e2.FirstName || ' ' || e2.LastName as reports_to 
+from Employee e left join Employee e2 on e.ReportsTo = e2.EmployeeId;
+
+-- we can get all the data from the "right" table whether or not there is a match
+select 
+	e.FirstName || ' ' || e.LastName as reportee, 
+	e2.FirstName || ' ' || e2.LastName as reports_to 
+from Employee e right join Employee e2 on e.ReportsTo = e2.EmployeeId;
+
+-- we can get all the data from both tables whether or not there is a match
+select 
+	e.FirstName || ' ' || e.LastName as reportee, 
+	e2.FirstName || ' ' || e2.LastName as reports_to 
+from Employee e full join Employee e2 on e.ReportsTo = e2.EmployeeId;
+
+
+-- we can match all records with all possible combinations between the two tables
+select 
+	e.FirstName || ' ' || e.LastName as reportee, 
+	e2.FirstName || ' ' || e2.LastName as reports_to 
+from Employee e cross join Employee e2;
+
+-- if you want to combine your data into shared columns you can use a union to
+-- accomplish this. Note here we store the first and last name of employees and customers
+-- in a single column for each name type
+select Firstname, Lastname from Employee
+union
+select FirstName, Lastname from Customer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
