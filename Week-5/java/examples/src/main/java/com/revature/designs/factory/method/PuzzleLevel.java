f5c6678a6f60277a6d6b2e440e9f3aa3b17dcdd8
EricTrainingRev <eric.suminski@revature.com>
2026-09-21 11:09:26 -0400

    adding design pattern initial examples

---
+++ Week-5/java/examples/src/main/java/com/revature/designs/factory/method/PuzzleLevel.java
@@ -0,0 +1,18 @@
+package com.revature.designs.factory.method;
+
+/**
+ * A puzzle level. Final, as required by the sealed contract.
+ */
+public final class PuzzleLevel extends Level {
+
+    private boolean isHard;
+
+    public PuzzleLevel(boolean isHard){
+        this.isHard = isHard;
+    }
+
+    @Override
+    public String play() {
+        return isHard ? "Starting a hard puzzle level" : "Starting an easy puzzle level";
+    }
+}
