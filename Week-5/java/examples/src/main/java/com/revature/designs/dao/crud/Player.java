cabb9533ec58dde62e592f0fc03aec2c29129a53
EricTrainingRev <eric.suminski@revature.com>
2026-09-21 13:06:09 -0400

    Adding a small comment to crud Player code

--- Week-5/java/examples/src/main/java/com/revature/designs/dao/crud/Player.java
+++ Week-5/java/examples/src/main/java/com/revature/designs/dao/crud/Player.java
@@ -3,6 +3,9 @@ package com.revature.designs.dao.crud;
 /**
  * A record is the ideal "plain data object" for a DAO to carry in and out: it
  * bundles the player's fields with zero boilerplate, and is immutable by design.
+ *
+ * NOTE: for the video game theme of these examples a record would not be an ideal choice
+ * for storing player data, it is just used here for convenience
  */
 public record Player(int id, String name, int level, int health, int gold) {
 }
